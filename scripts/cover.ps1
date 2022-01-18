f6c038b536e1895d4ae03990a57220c7a4b3fe92
naveen-variant <76699620+naveen-variant@users.noreply.github.com>
2022-01-18 15:51:47 -0500

    Feature/cloud 1412 coverlet msbuild (#23)
    * adding msbuild
    
    * adding image
    
    * adding coverlet
    
    * adding both msbuild and coverlet
    
    * minor fixes
    
    * ref v1.2.0

--- scripts/cover.ps1
+++ scripts/cover.ps1
@@ -28,6 +28,7 @@ $tests = $(dotnet sln $solutionFileDir list) | Select-Object -Skip 2 | Where-Obj
 $tests | ForEach-Object {
   $file = [System.IO.DirectoryInfo]"$_"
   $parent = $($file.parent.fullname)
+  ce dotnet add $parent package coverlet.msbuild
   ce dotnet add $parent package coverlet.collector
 }
 
