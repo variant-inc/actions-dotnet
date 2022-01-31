de40c89e4e7b9fb706ff639c03c6f77e06a9fc55
LeeHughsVariant <lhughs@usxpress.com>
2022-01-31 10:54:00 -0500

    Add quality wait flag

--- scripts/sonarscan.sh
+++ scripts/sonarscan.sh
@@ -8,9 +8,14 @@ mkdir -p "$OUTPUTDIR"
 SONAR_ORGANIZATION="$SONAR_ORG"
 
 sonar_logout() {
-  set +eu
+  set +u
   dotnet-sonarscanner end /d:sonar.login="$SONAR_TOKEN"
 }
+
+wait_flag="false"
+if [ "$BRANCH_NAME" == "master" ] || [ "$BRANCH_NAME" == "main" ]; then
+  wait_flag="true"
+fi
 
 sonar_args="/o:$SONAR_ORGANIZATION \
     /k:$SONAR_PROJECT_KEY \
@@ -19,7 +24,8 @@ sonar_args="/o:$SONAR_ORGANIZATION \
     /d:sonar.cs.opencover.reportsPaths=**/$OUTPUTDIR/**/coverage.opencover.xml \
     /d:sonar.exclusions=**/*Migrations/**/* \
     /d:sonar.scm.disabled=true \
-    /d:sonar.scm.revision=$GITHUB_SHA"
+    /d:sonar.scm.revision=$GITHUB_SHA \
+    /d:sonar.qualitygate.wait=$wait_flag"
 
 if [ "$PULL_REQUEST_KEY" = null ]; then
   eval "dotnet-sonarscanner $sonar_args /d:sonar.branch.name=$BRANCH_NAME"
