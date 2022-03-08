45a5cbe82330a793df4831e20273a3b7609226c2
Hitesh Patel <HiteshP@drivevariant.com>
2022-03-08 11:16:25 -0500

    better nuget package versioning

--- scripts/nuget_push.sh
+++ scripts/nuget_push.sh
@@ -6,7 +6,15 @@ cd "$GITHUB_WORKSPACE"
 
 mkdir -p out
 
-dotnet pack --no-restore -c Release --version-suffix "$IMAGE_VERSION" -o /out
+if [ "$GITVERSION_NUGETPRERELEASETAG" ]; then
+  BRANCH_BUILD_SUFFIX=${GITVERSION_NUGETPRERELEASETAG}-${GITHUB_RUN_NUMBER}
+  echo "NuGet Push: Pushing branch version $BRANCH_BUILD_SUFFIX"
+  dotnet pack --no-restore -c Release --version-suffix "${BRANCH_BUILD_SUFFIX}" -o /out
+else
+  echo "NuGet Push: Pushing release version from CSPROJ" 
+  dotnet pack --no-restore -c Release -o /out
+fi
+
 dotnet nuget push "/out/**/*.nupkg" --source github --skip-duplicate --api-key "$INPUT_NUGET_PUSH_TOKEN"
 
 rm -rf out
