ef5c2d4b48b6f4a639222216ac9e994f12b053cc
naveen-variant <naveenp@drivevariant.com>
2022-05-17 09:54:32 -0400

    sonar check

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,7 +18,7 @@ echo "Current directory: $(pwd)"
 git config --global --add safe.directory /github/workspace
 
 echo "Cloning into actions-collection..."
-git clone -b v1 https://github.com/variant-inc/actions-collection.git ./actions-collection
+git clone -b feature/CLOUD-1738-skip-sonar-analysis https://github.com/variant-inc/actions-collection.git ./actions-collection
 
 echo "---Start: Pretest script"
 chmod +x ./actions-collection/scripts/pre_test.sh
@@ -41,26 +41,37 @@ echo "Start: Enable sonar"
 pwsh ./actions-collection/scripts/enable_sonar.ps1
 echo "End: Enable sonar"
 
-echo "Start: Sonar Scan"
-sh -c "/scripts/coverage_scan.sh"
-echo "End: Sonar Scan"
+echo "Start: Check sonar run"
+skip_sonar_run=$(pwsh ./actions-collection/scripts/skip_sonar_run.ps1)
+echo "Skip sonar run: $skip_sonar_run"
+echo "End: Check sonar run"
 
-echo "Container Push: $INPUT_CONTAINER_PUSH_ENABLED"
-if [ "$INPUT_CONTAINER_PUSH_ENABLED" = 'true' ]; then
-  echo "Start: Checking ECR Repo"
-  ./actions-collection/scripts/ecr_create.sh "$INPUT_ECR_REPOSITORY"
-  echo "End: Checking ECR Repo"
-  echo "Start: Publish Image to ECR"
-  ./actions-collection/scripts/publish.sh
-  echo "End: Publish Image to ECR"
+
+if [ "$skip_sonar_run" != 'True' ]; then
+  echo "Start: Sonar Scan"
+  sh -c "/scripts/coverage_scan.sh"
+  echo "End: Sonar Scan"
+else
+  echo "End: Skipping sonar run"
 fi
 
-echo "Nuget Publish: $INPUT_NUGET_PUSH_ENABLED"
-if [ "$INPUT_NUGET_PUSH_ENABLED" = 'true' ]; then
-  echo "Start: Publish Nuget Package"
-  /scripts/nuget_push.sh
-  echo "End: Publish Nuget Package"
-fi
+# echo "Container Push: $INPUT_CONTAINER_PUSH_ENABLED"
+# if [ "$INPUT_CONTAINER_PUSH_ENABLED" = 'true' ]; then
+#   echo "Start: Checking ECR Repo"
+#   ./actions-collection/scripts/ecr_create.sh "$INPUT_ECR_REPOSITORY"
+#   echo "End: Checking ECR Repo"
+#   echo "Start: Publish Image to ECR"
+#   ./actions-collection/scripts/publish.sh
+#   echo "End: Publish Image to ECR"
+# fi
+
+
+# echo "Nuget Publish: $INPUT_NUGET_PUSH_ENABLED"
+# if [ "$INPUT_NUGET_PUSH_ENABLED" = 'true' ]; then
+#   echo "Start: Publish Nuget Package"
+#   /scripts/nuget_push.sh
+#   echo "End: Publish Nuget Package"
+# fi
 
 echo "Start: Clean up"
 git clean -fdx
