2c0dfc14a71729e4ae7a5d187aab20ada21759d7
Tuan Nguyen <from.tuan2@gmail.com>
2022-06-08 15:02:12 -0500

    [hotfix] removed no-restore with explicit dotnet restore

--- scripts/nuget_push.sh
+++ scripts/nuget_push.sh
@@ -6,10 +6,12 @@ cd "$GITHUB_WORKSPACE"
 
 mkdir -p out
 
+dotnet restore
+
 if [ "$GITVERSION_NUGETPRERELEASETAG" ]; then
   BRANCH_BUILD_SUFFIX=${GITVERSION_NUGETPRERELEASETAG}-${GITHUB_RUN_NUMBER}
   echo "NuGet Push: Pushing branch version $BRANCH_BUILD_SUFFIX"
-  dotnet pack --no-restore -c Release --version-suffix "${BRANCH_BUILD_SUFFIX}" -o /out
+  dotnet pack -c Release --version-suffix "${BRANCH_BUILD_SUFFIX}" -o /out
 else
   echo "NuGet Push: Pushing release version from CSPROJ" 
   dotnet pack -c Release -o /out
