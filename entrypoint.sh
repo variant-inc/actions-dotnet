941c2c9a163873b74c37080d242dab1bff23f1e3
Tuan Nguyen <from.tuan2@gmail.com>
2022-06-16 16:52:26 -0500

    [CLOUD-1741] reverted publish.sh and pointed to actions-collections branch

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,7 @@ echo "Current directory: $(pwd)"
 git config --global --add safe.directory /github/workspace
 
 echo "Cloning into actions-collection..."
-git clone -b v1 https://github.com/variant-inc/actions-collection.git ./actions-collection
+git clone -b f/CLOUD-1741 https://github.com/variant-inc/actions-collection.git ./actions-collection
 
 echo "---Start: Pretest script"
 chmod +x ./actions-collection/scripts/pre_test.sh
