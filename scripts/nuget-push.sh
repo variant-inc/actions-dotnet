b7094869116df60bed7d569cb1058b0a1dd9bed3
Cameran20 <clai@usxpress.com>
2023-05-24 15:49:08 -0400

    move scripts and add summary

--- scripts/nuget-push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -l
-
-set -eu
-
-mkdir -p out
-dotnet pack -c Release -o /out  --version-suffix "$IMAGE_VERSION"
-dotnet nuget push "/out/**/*.nupkg" --source github --skip-duplicate --api-key "$GITHUB_TOKEN"
-
-rm -rf out
